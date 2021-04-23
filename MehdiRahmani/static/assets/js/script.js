505f953a2a76d09ac43040720200fd259087ed4a
mahdi rahmani <mehdirahmani314@gmail.com>
2021-04-24 01:28:20 +0430

    add header

--- MehdiRahmani/static/assets/js/script.js
+++ MehdiRahmani/static/assets/js/script.js
@@ -1,3 +1,4 @@
+// start slider
 var swiper = new Swiper('.swiper3', {
   slidesPerGroup: 5,
   speed: 1000,
@@ -16,3 +17,32 @@ var swiper = new Swiper('.swiper3', {
     }
   }
 });
+// end slider
+
+//  start header
+
+// start search
+const search = document.querySelector('#search');
+
+function toggleSearchResault(event) {
+  const serachResult = document.querySelectorAll('.header__serach-result')[0];
+  const serachInput = document.querySelectorAll('.header__serach-input')[0];
+  const inputValue = event.target.value;
+  if (event.type == 'click') {
+    serachResult.style = "display: none";
+    serachInput.classList.remove('header__serach-input--is-active');
+  } else {
+    serachResult.style = "display: block";
+    serachInput.classList.add('header__serach-input--is-active');
+  }
+}
+
+search.addEventListener('keyup', toggleSearchResault);
+
+const overlay = document.querySelectorAll('.overlay')[0];
+overlay.addEventListener('click', function (event) {
+  toggleSearchResault(event);
+});
+// end search
+
+// end header
